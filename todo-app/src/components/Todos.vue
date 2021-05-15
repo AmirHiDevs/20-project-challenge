ef7d7be101306f71c9b75c415fe1181cf2feb083
ChanceBurger <chncbrgr@gmail.com>
2021-05-15 09:02:38 +0430

    initial commit

---
+++ todo-app/src/components/Todos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>My to-do list</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for="todo in todos">
+        <Todo v-bind:todo="todo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Todo from "./Todo";
+
+export default {
+  name: "Todos",
+  components: {
+    Todo
+  },
+ 
+  props:{
+      todos : {
+          type:Array,
+          // eslint-disable-next-line vue/require-valid-default-prop
+          default:[]
+      } 
+  }
+}
+
+</script>
+
+<style></style>
---
+++ todo-app/src/components/addTodo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input type="text" v-model="title" name="title" />
+    <button type="submit" @click="addTodo">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+
+  methods: {
+    randomId() {
+      return Math.ceil(Math.random() * 100000);
+    },
+    addTodo() {
+      const newTodo = {
+        id: this.randomId(),
+        title: this.title,
+        completed: false,
+      };
+
+      this.$emit('emit-Addtodo',newTodo);
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style></style>
